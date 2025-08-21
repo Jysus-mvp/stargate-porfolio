e832067b9f5c9896c98cf74e55cd9963c40f0da4
Jysus <137215542+Jysus-mvp@users.noreply.github.com>
2025-08-21 07:50:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//codigo para el modal 1/4
+const abrirModal_1 = document.getElementById('abrirModal_1')
+const events_info_1 = document.querySelector('.events-info_1')
+ 
+const mostarModal_1 = ()=>{
+    events_info_1.showModal()
+}
+const cerrarModal_1 = ()=>{
+    events_info_1.close()
+}
+
+//codigo para el modal 2/4
+const abrirModal_2 = document.getElementById('abrirModal_2')
+const events_info_2 = document.querySelector('.events-info_2')
+ 
+const mostarModal_2 = ()=>{
+    events_info_2.showModal()
+}
+const cerrarModal_2 = ()=>{
+    events_info_2.close()
+}
+
+
+//codigo para el modal 3/4
+const abrirModal_3 = document.getElementById('abrirModal_3')
+const events_info_3 = document.querySelector('.events-info_3')
+
+const mostarModal_3 = ()=>{
+    events_info_3.showModal()
+}
+const cerrarModal_3 = ()=>{
+    events_info_3.close()
+}
+
+//codigo para el modal 4/4
+const abrirModal_4 = document.getElementById('abrirModal_4')
+const events_info_4 = document.querySelector('.events-info_4')
+ 
+const mostarModal_4 = ()=>{
+    events_info_4.showModal()
+}
+const cerrarModal_4 = ()=>{
+    events_info_4.close()
+}
